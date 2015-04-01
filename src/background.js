e5331eb859535f631cc41deb56cf78d0d744730a
Ben Lau <xbenlau@gmail.com>
2015-04-01 15:47:55 +0800

    Block Javascript from specific websites

---
+++ src/background.js
@@ -0,0 +1,17 @@
+
+var filterUrls = [
+  "*://*.baidu.com/*.js",
+  "*://*.baidustatic.com/*.js"   
+];
+
+chrome.webRequest.onBeforeRequest.addListener(function(info) {
+    
+        console.log("Blocked: " , info.url);
+    
+        return { cancel : true};
+    },{
+        urls: filterUrls,
+        types: ["script","image"]        
+    }
+                                             
+ );
