4deeaee967e8f37a7847e529c95629272c2bd3e1
Ben Lau <xbenlau@gmail.com>
2015-04-03 00:17:39 +0800

    Prevent to load greatfire and cn-nytimes as script/image

--- src/background.js
+++ src/background.js
@@ -2,7 +2,13 @@
 var filterUrls = [
   "*://*.baidu.com/*.js*",
   "*://*.baidustatic.com/*.js*",
-  "*://sjs.sinajs.cn/blog7common/js/boot.js"
+  "*://sjs.sinajs.cn/blog7common/js/boot.js",
+    
+
+  /* Vicitm. It won't block direct access. 
+    It just block request as script and image */
+  "*://*.github.com/greatfire",
+  "*://*.github.com/cn-nytimes"    
 ];
 
 chrome.webRequest.onBeforeRequest.addListener(function(info) {
